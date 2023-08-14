1e9fbe768bc5bf5c9329d82fbac4d1f9f27707ec
Joshua Lawson <Joshua.lawson@sjsu.edu>
2023-08-13 19:12:14 -0700

    redux persist init

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './store';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>
 );
 
--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,28 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import pointsListSlice from './features/pointsList/pointsListSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+
+const customizedMiddleware = getDefaultMiddleware({
+    serializableCheck: false
+})
+
 const rootReducer = combineReducers({
     pointsListSlice
 })
 
-export default configureStore({
-    reducer: rootReducer,
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: customizedMiddleware
+
 })
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof rootReducer>
 
